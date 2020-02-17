5796b4be062fd4d238210dbb9b6c4ac7830454ec
Ruth Obe <ruthobey@gmail.com>
2020-02-17 21:56:54 +0000

    Automatically backed up by Learn

--- lib/iteration.rb
+++ lib/iteration.rb
@@ -1,4 +1,4 @@
-def join_ingredients(src)
+# def join_ingredients(src)
   # Given an Array of 2-element Arrays ( [ [food1, food2], [food3, # food4]....[foodN, foodM]]):
   #
   # Build a new Array that contains strings where each pair of foods is
@@ -6,20 +6,50 @@ def join_ingredients(src)
   #
   # "I love (inner array element 0) and (inner array element 1) on my pizza""
   # As such, there should be a new String for each inner array, or pair
-end
+  
+  array_1 = [['pepperoni', 'sausage'], ['green olives', 'green peppers'], ['onions', 'pineapple']]
+  
+  
+  row_index = 0 
+  
+  while row_index < array_1.count do 
+    
+    element_index = 0 
+    
+    while element_index < array_1[row_index].count  do 
+      
+    
+      p "I love #{array_1[row_index][element_index]} and #{array_1[row_index][element_index + 1]} on my pizza"
+      
 
-def find_greater_pair(src)
-  # src will be an array of [ [number1, number2], ... [numberN, numberM] ]
-  # Produce a new Array that contains the larger number of each of the pairs
-  # that are in the inner Arrays
-end
+    
+      element_index += 1 
+    end 
+    
+    # p array_1[row_index]
+  
+    row_index += 1 
+    
+  end 
+  
+# end
 
-def total_even_pairs(src)
-  # src will be an array of [ [number1, number2], ... [numberN, numberM] ]
-  # if both numbers in the pair are even, then add both those numbers to the
-  # total
-  #
-  # As a reminder any number % 2 will return 0 or 1. If the result is 0, then
-  # the number was even. Review the operator documentation if you've forgotten
-  # this!
-end
+#--------------------------------
+
+# def find_greater_pair(src)
+#   # src will be an array of [ [number1, number2], ... [numberN, numberM] ]
+#   # Produce a new Array that contains the larger number of each of the pairs
+#   # that are in the inner Arrays
+# end
+
+# #--------------------------------
+
+# def total_even_pairs(src)
+#   # src will be an array of [ [number1, number2], ... [numberN, numberM] ]
+#   # if both numbers in the pair are even, then add both those numbers to the
+#   # total
+#   #
+#   # As a reminder any number % 2 will return 0 or 1. If the result is 0, then
+#   # the number was even. Review the operator documentation if you've forgotten
+#   # this!
+# end
